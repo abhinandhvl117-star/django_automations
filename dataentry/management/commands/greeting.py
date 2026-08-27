afb30f3d0403f2ee34c16373062e97a67fc7ba08
Abhinandh vijayan <abhinandhvl117@gmail.com>
2026-08-27 20:43:12 +0530

    created custom commands

---
+++ dataentry/management/commands/greeting.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "greets the user"
+    
+    def add_arguments(self, parser):
+        parser.add_argument('name', type=str, help="gets user name")
+    
+    def handle(self, *args, **kwargs):
+        name = kwargs['name']
+        greeting = f'Hello {name}, Good Morning'
+        # normal
+        # self.stdout.write(greeting)
+        self.stdout.write(self.style.HTTP_REDIRECT(greeting))
